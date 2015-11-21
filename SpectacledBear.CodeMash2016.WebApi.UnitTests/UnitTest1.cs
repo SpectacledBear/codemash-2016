04606eb28a18d21f3ab08099fff1267bfeef3fee
Kyle Lewis <thekyle@gmail.com>
2015-11-21 12:46:56 -0500

    Adding default blank Web API solution

---
+++ SpectacledBear.CodeMash2016.WebApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpectacledBear.CodeMash2016.WebApi.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SpectacledBear.CodeMash2016.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpectacledBear.CodeMash2016.WebApi.WebApiApplication" Language="C#" %>
