bb68dfe5243697f27b670e7396d40d67df2599a4
Kyle Lewis <thekyle@gmail.com>
2015-11-21 19:24:16 -0500

    Adding in-memory SQLite database and monitoring API

---
+++ SpectacledBear.CodeMash2016.WebApi/Controllers/MonitoringController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using SpectacledBear.CodeMash2016.WebApi.Data;
+using SpectacledBear.CodeMash2016.WebApi.Models;
+
+namespace SpectacledBear.CodeMash2016.WebApi.Controllers
+{
+    public class MonitoringController : ApiController
+    {
+        SqliteDataManager _sqliteData = new SqliteDataManager();
+
+        // GET: api/Monitoring
+        public SqliteModel Get()
+        {
+            SqliteModel model = _sqliteData.GetModels();
+
+            return model;
+        }
+
+        // GET: api/Monitoring/5
+        public string Get(int id)
+        {
+            throw new NotImplementedException("This method has not been written yet.");
+        }
+
+        // POST: api/Monitoring
+        public void Post([FromBody]string value)
+        {
+            throw new NotImplementedException("This method has not been written yet.");
+        }
+
+        // PUT: api/Monitoring/5
+        public void Put(int id, [FromBody]string value)
+        {
+            throw new NotImplementedException("This method has not been written yet.");
+        }
+
+        // DELETE: api/Monitoring/5
+        public void Delete(int id)
+        {
+            throw new NotImplementedException("This method has not been written yet.");
+        }
+    }
+}
---
+++ SpectacledBear.CodeMash2016.WebApi/Models/SqliteModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SpectacledBear.CodeMash2016.WebApi.Models
+{
+    public class SqliteModel
+    {
+        public string Version { get; private set; }
+        public IEnumerable<string> Tables { get; private set; }
+
+        public SqliteModel(string version, IEnumerable<string> tables)
+        {
+            Version = version;
+            Tables = tables;
+        }
+    }
+}
