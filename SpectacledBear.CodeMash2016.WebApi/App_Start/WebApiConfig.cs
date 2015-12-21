fde22425dac127935416016affc5e24a1b3393a7
Kyle Lewis <thekyle@gmail.com>
2015-12-20 21:35:43 -0500

    Changing text/html output to JSON The default serializer for the XML formatter cannot read my model objects, but the JSON formatter can.

--- SpectacledBear.CodeMash2016.WebApi/App_Start/WebApiConfig.cs
+++ SpectacledBear.CodeMash2016.WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace SpectacledBear.CodeMash2016.WebApi
@@ -10,6 +11,7 @@ namespace SpectacledBear.CodeMash2016.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
