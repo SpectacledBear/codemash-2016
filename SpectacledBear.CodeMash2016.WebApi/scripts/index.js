629bea4f6c2fdc4ae323ef5dc2dc35947529d148
Kyle Lewis <thekyle@gmail.com>
2015-12-26 15:25:59 -0500

    Index page now queries Hobbits from web service

---
+++ SpectacledBear.CodeMash2016.WebApi/scripts/index.js
@@ -0,0 +1,18 @@
+﻿function loadData() {
+    var items = [];
+
+    $.getJSON("http://localhost:11341/api/hobbit", function (data) {
+        $.each(data, function (key, val) {
+            var lineItem = "<tr>\n" + 
+                "<td>" + val.Name + "</td>\n" +
+                "<td>" + val.FamilyName + "</td>\n" +
+                "<td>" + val.BirthYear + "</td>\n" +
+                "<td>" + val.DeathYear + "</td>\n";
+            $("#hobbitsList").append(lineItem);
+        });
+    });
+};
+
+$(document).ready(function () {
+    loadData();
+});
