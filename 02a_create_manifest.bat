0fa39bca56df88d94a36d523331bcb21deb170d0
Kyle Lewis <thekyle@gmail.com>
2016-05-14 10:37:33 -0400

    Switching to relative paths for manifest test

--- 02a_create_manifest.bat
+++ 02a_create_manifest.bat
@@ -1,1 +1,1 @@
-SpectacledBear.CodeMash2016.Manifest\bin\Release\Manifest.exe -build -directory "C:\Users\Kyle\Documents\BitBucket\codemash-2016\SpectacledBear.CodeMash2016.WebApi\bin" -file "manifest.json"
+SpectacledBear.CodeMash2016.Manifest\bin\Release\Manifest.exe -build -directory ".\SpectacledBear.CodeMash2016.WebApi\bin" -file "manifest.json"
--- 02b_compare_manifest.bat
+++ 02b_compare_manifest.bat
@@ -1,1 +1,1 @@
-SpectacledBear.CodeMash2016.Manifest\bin\Release\Manifest.exe -check -file "manifest.json" -directory "C:\Users\Kyle\Documents\BitBucket\codemash-2016\SpectacledBear.CodeMash2016.WebApi\bin" -report "report.json"
+SpectacledBear.CodeMash2016.Manifest\bin\Release\Manifest.exe -check -file "manifest.json" -directory ".\SpectacledBear.CodeMash2016.WebApi\bin" -report "report.json"
